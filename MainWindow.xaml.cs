50cb37d00fec129af8bc1b89e20af36b2d06b528
Dmitry Kravchenko <dims12@mail.ru>
2018-05-26 20:53:12 +0300

    Recreated in separate directory

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace NormalKeyboardSwitcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private KeyboardListener keyboardListener;
+        private ForegroundWindowListener foregroundWindowListener;
+        private InputController inputController;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            inputController = new InputController();
+            keyboardListener = new KeyboardListener(FirstKey.Control);
+            foregroundWindowListener = new ForegroundWindowListener();
+
+            // when languange change key combinations are pressed then call appropriate methods of input controller
+            keyboardListener.NextTemporaryInputLanguage += inputController.NextTemporaryInputLanguage;
+            keyboardListener.SwitchToTemporaryInputLanguage += inputController.SwitchToTemporaryInputLanguage;
+            keyboardListener.DropTemporaryInputLanguage += inputController.DropTemporaryInputLanguage;
+
+            // if input language is changed in input controller, then send it to foreground window
+            inputController.InputLanguageChanged += foregroundWindowListener.InputLangChangeRequest;
+
+            // if foreground window changed, then send to it a request to change to current language from the controller
+            foregroundWindowListener.ForegroundWindowChanged += (hwnd) => { foregroundWindowListener.InputLangChangeRequest(hwnd, inputController.UsedInputLanguage); };
+
+            // binds list of system languages to a list box in GUI
+            LanguagesListBox.ItemsSource = inputController.UsedInputLanguages;
+
+            Binding TemporaryInputLanguageBinding = new Binding("TemporaryInputLanguage");
+            TemporaryInputLanguageBinding.Mode = BindingMode.OneWay;
+            TemporaryInputLanguageBinding.Source = inputController;
+            LanguagesListBox.SetBinding(ListBox.SelectedIndexProperty, TemporaryInputLanguageBinding);
+
+
+            
+            
+        }
+
+
+
+
+        private void SwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(LanguagesListBox.SelectedIndex>=0)
+            {
+                inputController.SwitchToLanguage(LanguagesListBox.SelectedIndex);
+            }
+        }
+    }
+}
